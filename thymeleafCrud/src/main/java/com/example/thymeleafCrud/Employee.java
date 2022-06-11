79af17e3719ce3715e85a12d78864829448f6ed3
mszgajewski <mszgajewski@gmail.com>
2022-06-11 23:37:18 +0200

    Initial Commit

--- thymeleafCrud/src/main/java/com/example/thymeleafCrud/Employee.java
+++ thymeleafCrud/src/main/java/com/example/thymeleafCrud/Employee.java
@@ -1,5 +1,10 @@
 package com.example.thymeleafCrud;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Employee {
 
     private String id;
@@ -18,37 +23,4 @@ public class Employee {
         this.lastName = lastName;
         this.age = age;
     }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
-
--- thymeleafCrud/src/main/java/com/example/thymeleafCrud/EmployeeController.java
+++ thymeleafCrud/src/main/java/com/example/thymeleafCrud/EmployeeController.java
@@ -13,58 +13,64 @@ import java.util.Map;
 @Controller
 public class EmployeeController {
 
-    public Map<String,Employee> empMap = new HashMap<>();
+    public Map<String, Employee> empMap = new HashMap<>();
 
     public EmployeeController() {
-        empMap.put("E101", new Employee("E101", "Lukasz", "Lee", 32));
-        empMap.put("E102", new Employee("E102", "Marcin", "Gayle", 33));
-        empMap.put("E103", new Employee("E103", "Piotrek", "Lara", 23));
-        empMap.put("E104", new Employee("E104", "Slawek", "Dhoni", 34));
+        empMap.put("E101", new Employee("1", "Brett", "Lee", 23));
+        empMap.put("E102", new Employee("2", "Chris", "Gayle", 23));
+        empMap.put("E103", new Employee("3", "Brian", "Lara", 23));
+        empMap.put("E104", new Employee("4", "MS", "Dhoni", 23));
 
     }
 
     @GetMapping("/employee")
     public String getAllEmployees(Model model) {
+
         model.addAttribute("employees", empMap.values());
+
         return "employee-list";
     }
 
+    // to get employee form
     @GetMapping("/employee/add")
     public String addEmployee() {
+
         return "employee-form";
     }
 
+    // to add new employee
     @PostMapping("/employee/add")
     public String addEmployee(@ModelAttribute("addEmployee") Employee employee, Model model) {
-        empMap.put(employee.getId(),employee);
+        empMap.put(employee.getId(), employee);
         model.addAttribute("employees", empMap.values());
         return "employee-list";
     }
 
+    // get employee to edit
     @GetMapping("/employee/edit/{id}")
     public String getAllEmployees(Model model, @PathVariable("id") String empId) {
+
         model.addAttribute("editEmployee", empMap.get(empId));
 
         return "employee-edit";
     }
 
+    // to update edited employee
     @PostMapping("/employee/edit/{id}")
-    public String updateEmployee(@ModelAttribute ("editEmployee") Employee employee, @PathVariable("id")
-    String empId,Model model) {
-        empMap.put(empId,employee);
+    public String updateEmployee(@ModelAttribute("editEmployee") Employee employee, @PathVariable("id") String empId,
+                                 Model model) {
+
+        empMap.put(empId, employee);
+
         model.addAttribute("employees", empMap.values());
         return "employee-list";
     }
 
+    // to delete particular employee
     @GetMapping("/employee/delete/{id}")
-    public String deleteEmployee(Model model, @PathVariable("'id") String empId) {
+    public String deleteEmployee(Model model, @PathVariable("id") String empId) {
         empMap.remove(empId);
-        model.addAttribute("employees",empMap.values());
+        model.addAttribute("employees", empMap.values());
         return "employee-list";
     }
-
-
-
-
-
 }
